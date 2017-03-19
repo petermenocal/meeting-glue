f6aa82638254904cf3bf889ab48ebbdeb9092f6f
Peter Menocal <peter@geekypixel.com>
2017-03-19 13:03:31 -0700

    build 0.0.2

--- bin/app.js
+++ bin/app.js
@@ -1772,7 +1772,7 @@ module.exports = {
   view: () => {
     return m("div", {class: "bg-white pa4"}, [
       m("h1", {class: "avenir fw5 green ttu"}, "Latest Stories"),
-      m("div#carousel", {class: "flex flex-row", style: "max-height: 400px; width: 100%; overflow-x: scroll; overflow-y: hidden"}, Article.list.map((article)=>{
+      m("div#carousel", {class: "flex flex-row", style: "max-height: 400px; width: 100%; overflow-x: scroll; overflow-y: hidden; -webkit-overflow-scrolling: touch"}, Article.list.map((article)=>{
         return m("div", {class: "mr4", style: "min-width: 200px"}, [
           m("img", {class: "fl w-100", src: "//placehold.it/400x400"}),
           m("h1", {class: "avenir green fl f4 fw4 w-100 mb0"}, article.title),
