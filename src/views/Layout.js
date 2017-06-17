79e9c36974744ba59f3eef9d8428c90a72d1705b
Peter Menocal <peter@geekypixel.com>
2017-06-17 01:46:11 -0700

    Globalize navigation inside layout shell.

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -5,6 +5,7 @@ module.exports = {
   view: (vnode) => {
     return m("div", {class: "fl w-100 h-100 vh-100 bg-near-white "}, [
       m("div", {class: "fl vh-100 h-100 w-100"}, [
+        m(Nav),
         m("div", {class: "fl w-100 h-100 bg-near-white", style: "z-index: 11"}, vnode.children)
       ])
     ])
