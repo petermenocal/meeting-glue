bb1114220e4be387b0d6f90fcb4440d5758ab899
Peter Menocal <peter@geekypixel.com>
2017-06-17 03:53:03 -0700

    Adjust webpack config for Twitter API

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,18 @@
 module.exports = {
   entry: "./src/index.js",
-  output: { filename: "./bin/app.js" }
+  output: { filename: "./bin/app.js" },
+  module: {
+   loaders: [
+     { test: /\.json$/, loader: 'json-loader' }
+   ]
+ },
+ resolve: {
+   extensions: ['.webpack.js', '.web.js', '.js']
+ },
+ node: {
+    console: 'mock',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  }
 }
